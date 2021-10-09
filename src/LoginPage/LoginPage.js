064f5255f51c70475a11d647dcd38bab74c2af9e
Anuj <anujjoshi63@gmail.com>
2021-10-09 13:55:23 +0530

    1st proto of LoginPage

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,34 +1,38 @@
 import './LoginPage.css';
+import logo from '../assets/images/tiwpe-logo.png';
+import { useHistory } from 'react-router';
 
-const LoginPage = ({ handleClose, modalShow }) => {
-  const showHideClassName = modalShow
-    ? 'modal display-block'
-    : 'modal display-none';
+const LoginPage = () => {
+  const history = useHistory();
+  const handleLogin = () => {
+    history.push('/dashboard');
+  };
 
   return (
-    <div className={showHideClassName}>
-      <div className="modal-main">
-        <div id="modal-login-image">
-          <img
-            src="https://images.unsplash.com/photo-1593672715438-d88a70629abe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80"
-            alt=""
-            height="100%"
-            style={{
-              borderRadius: '10px 00px 0px 10px'
+    <div id="login-background">
+      <div id="login-page-content">
+        <div id="login-logo">
+          <img src={logo} alt="Brand Logo" />
+        </div>
+        <div id="login-box">
+          <span id="login-welcome-text">Welcome to TIW Private Equity</span>
+          <span id="login-login-type">Nominee Login</span>
+          <span className="login-input-helper">User name</span>
+          <input type="text" />
+          <span className="login-input-helper">Password</span>
+          <input type="password" name="" id="" />
+          <span
+            id="login-forgot-password"
+            onClick={() => {
+              alert('action for forgot password');
             }}
-          />
-        </div>
-        <div id="modal-content">
-          <span id="help-text">Login to continue</span>
-          <span id="login-inputs">
-            <input type="text" placeholder="Email ID" />
-            <input type="text" placeholder="Password" />
+          >
+            Forgot Password?
           </span>
-          <button id="submit-button">Login</button>
+          <button id="login-submit-button" onClick={handleLogin}>
+            Login
+          </button>
         </div>
-        <button type="button" onClick={handleClose} id="modal-close-button">
-          ×
-        </button>
       </div>
     </div>
   );
