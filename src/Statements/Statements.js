fa01262891c424ef475e60f3ebb70c72434b0ac2
Ayush Raj <ayushsinghju7488@gmail.com>
2021-10-13 21:12:31 +0530

    Statement page added

--- src/Statements/Statements.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Statements.css';
-
-const Statements = () => {
-  return <div>Statements</div>;
-};
-
-export default Statements;
---
+++ src/routes.js
@@ -0,0 +1,4 @@
+export const LOGIN_ROUTE = "/";
+export const DASHBOARD_ROUTE = "/dashboard";
+export const PROFILE_ROUTE = "/profile";
+export const STATEMENTS_ROUTE = "/statements";
