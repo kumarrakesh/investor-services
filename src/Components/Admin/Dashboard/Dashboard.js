57c1f30e79f3d6ead02694197914904b7309c1db
Anuj <anujjoshi63@gmail.com>
2021-10-20 16:40:08 +0530

    redicect from admin to normal dashboard if not admin

--- src/Components/Admin/Dashboard/Dashboard.js
+++ src/Components/Admin/Dashboard/Dashboard.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import DashContainer from '../../User/Dashboard/DashboardContainer';
 import AdNavbar from '../Navbar/Navbar';
+import { UserContext } from '../../../userContext';
+import { useHistory } from 'react-router';
 
 const AdminDashboard = () => {
+  const { userData } = useContext(UserContext);
+  const history = useHistory();
+  useEffect(() => {
+    // console.log(userData);
+    if (userData.role !== 'ADMIN') history.replace('/dashboard');
+  }, []);
   return (
     <div id="header-container" style={{ display: 'flex' }}>
       <div>
