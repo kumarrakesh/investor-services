8cf96f16f2b5cc761f66921662e108cbeed21ff8
Anuj <anujjoshi63@gmail.com>
2021-10-26 20:49:51 +0530

    implement react-chartjs

--- src/Components/User/Dashboard/DashboardContainer.js
+++ src/Components/User/Dashboard/DashboardContainer.js
@@ -1,121 +1,95 @@
 import React from 'react';
 import './Dashboard.css';
-import Button from '@mui/material/Button';
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend
-} from 'recharts';
-
-const data = [
-  {
-    name: 'Jan',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: 'Feb',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: 'Mar',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: 'Apr',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: 'May',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: 'Jun',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: 'Jul',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  },
-  {
-    name: 'Aug',
-    uv: 3290,
-    pv: 4000,
-    amt: 2900
-  },
-  {
-    name: 'Sep',
-    uv: 3390,
-    pv: 4500,
-    amt: 4800
-  },
-  {
-    name: 'Oct',
-    uv: 3490,
-    pv: 6500,
-    amt: 2100
-  },
-  {
-    name: 'Nov',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  },
-  {
-    name: 'Dec',
-    uv: 5690,
-    pv: 1200,
-    amt: 2100
-  }
-];
+// import { Button } from '@mui/material';
+import { Line } from 'react-chartjs-2';
 
 const DashContainer = () => {
+  const state = {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'Novemeber',
+      'December'
+    ],
+    datasets: [
+      {
+        label: 'Investment',
+        fill: false,
+        backgroundColor: '#16D112',
+        borderColor: '#16D112',
+        borderWidth: 2,
+        // data: monthlyInvestment
+        data: [1, 2, 3, 4, 5]
+      },
+      {
+        label: 'Current Value',
+        fill: false,
+        backgroundColor: '#F98701',
+        borderColor: '#F98701',
+        borderWidth: 2,
+        // data: monthlyCurrentValue
+        data: [6, 7, 8, 9, 10]
+      },
+      {
+        label: 'Net Gain/Loss',
+        fill: false,
+        backgroundColor: '#1196FF',
+        borderColor: '#1196FF',
+        borderWidth: 2,
+        // data: monthlyNetAmount
+        data: [11, 12, 13, 14, 100]
+      }
+    ]
+  };
   return (
     <div className="dash-container">
-      <h1 className="dtitle">Dashboard</h1>
+      <div className="dashboard-title">Dashboard</div>
+      {/* <h1 className="dashboard-subtitle">Overview</h1> */}
 
-      <h1 className="overview">Overview</h1>
-
-      <div className="chart_outer">
-        <div className="chart_inner">
-          <LineChart
-            align={'center'}
-            width={800}
-            height={300}
-            data={data}
-            margin={{
-              top: 5,
-              right: 80,
-              left: 80,
-              bottom: 5
+      <div className="dashboard-graph-container">
+        <div className="dashboard-graph-info">
+          <div className="dashboard-graph-info-data">
+            <div className="dashboard-graph-info-data-label">
+              Total Investment
+            </div>
+            <div className="dashboard-graph-info-data-value">$10000.00</div>
+          </div>
+          <div className="dashboard-graph-info-data">
+            <div className="dashboard-graph-info-data-label">Current Value</div>
+            <div className="dashboard-graph-info-data-value">$15000.00</div>
+          </div>
+          <div className="dashboard-graph-info-data">
+            <div className="dashboard-graph-info-data-label">Net Gain/Loss</div>
+            <div className="dashboard-graph-info-data-value">$5000.00</div>
+          </div>
+          <button
+            onClick={() => {
+              alert('functionality to be added');
             }}
           >
-            {/* <CartesianGrid strokeDasharray="3 3" /> */}
-            <XAxis dataKey="name" />
-            <YAxis type="number" domain={[0, 'dataMax']} />
-            <Tooltip />
-            <Legend />
-            <Line dataKey="pv" stroke="#16D112" activeDot={{ r: 4 }} />
-            <Line dataKey="uv" stroke="#F98701" />
-            <Line stroke="#1196FF" />
-          </LineChart>
+            YEAR select button
+          </button>
+        </div>
+        <div className="dashboard-graph">
+          <Line
+            data={state}
+            options={{
+              title: {
+                display: true,
+                text: 'dashboard',
+                fontSize: 20,
+                color: 'red'
+              }
+            }}
+          />
         </div>
       </div>
 
