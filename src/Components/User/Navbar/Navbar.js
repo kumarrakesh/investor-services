7831f42bf971c26fc3addf2c1c84faf9b0afba6c
Anuj <anujjoshi63@gmail.com>
2021-12-20 20:33:58 +0530

    new changes

--- src/Components/User/Navbar/Navbar.js
+++ src/Components/User/Navbar/Navbar.js
@@ -66,11 +66,11 @@ const Navbar = () => {
     }).then((result) => {
       if (result.isConfirmed) {
         localStorage.clear();
-        Swal.mixin({
-          toast: true,
-          position: 'top-end',
-          timer: 2000
-        }).fire('You have been logged out', '', 'success');
+        // Swal.mixin({
+        //   toast: true,
+        //   position: 'top-end',
+        //   timer: 2000
+        // }).fire('You have been logged out', '', 'success');
         history.push('/');
       }
     });
